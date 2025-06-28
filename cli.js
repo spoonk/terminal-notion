6314ed2be462405b042c88caf2f04e07cbe7dd5f
spoonk <cornwlucas@gmail.com>
2025-06-28 12:02:21 -0700

    boilerplate setup

---
+++ cli.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { render, Text } from "ink";
+const Demo = () => /*#__PURE__*/React.createElement(Text, null, "Hello World");
+render(/*#__PURE__*/React.createElement(Demo, null));
